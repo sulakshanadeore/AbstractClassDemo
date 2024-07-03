6810b9b6b989f9ab9c98df5b6894a396481e2a95
deoresulakshana <deoresulakshana@gmail.com>
2024-07-03 14:08:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HRLibrary;
+namespace AbstractClassDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Clerk mgr=new Clerk();
+            //mgr.CalculateSalary(10000f, 2000f);
+
+            SalesMan salesMan = new SalesMan();
+            salesMan.Empid = 1;
+            salesMan.EmpName = "Hari";
+            salesMan.Deptno = 2;
+
+            FoodProducts food=new FoodProducts();
+            food.SellProduct(salesMan);
+
+            
+
+            Console.Read();
+        }
+    }
+}
